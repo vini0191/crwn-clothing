c539d53a7cea58dcbf3d5c6ab406a0c6ce8fe476
Vinicius Alvim <vini0191@gmail.com>
2020-07-16 17:10:06 -0300

    Implement code that pulls in shop data from firestore and stores it into the shop reducer from shop page. Also includes withSpinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -10,11 +10,10 @@ export const selectCollections = createSelector(
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   // (collections) => Object.keys(collections).map((key) => collections[key])
-  (collections) => Object.values(collections)
+  (collections) => (collections ? Object.values(collections) : [])
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   );
---
+++ src/redux/shop/shop.types.js
@@ -0,0 +1,5 @@
+const ShopActionTypes = {
+  UPDATE_COLLECTIONS: 'UDPATE_COLLECTIONS',
+};
+
+export default ShopActionTypes;
